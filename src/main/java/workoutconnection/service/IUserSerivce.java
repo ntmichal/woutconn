820279393f442e30e9038030f0ea89db36eafa49
Michał <nt.michal@gmail.com>
2018-09-13 17:15:37 +0200

    clean up

--- src/main/java/workoutconnection/service/IUserSerivce.java
+++ src/main/java/workoutconnection/service/IUserSerivce.java
@@ -2,14 +2,6 @@ package workoutconnection.service;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
 import workoutconnection.entities.User;
 
 
