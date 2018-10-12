70b7cfeb36d8e386fe944b397cf869d604ffb01d
Michał <nt.michal@gmail.com>
2018-10-12 16:34:44 +0200

    clean up

--- src/main/java/workoutconnection/Application.java
+++ src/main/java/workoutconnection/Application.java
@@ -4,9 +4,15 @@ package workoutconnection;
 
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 
@@ -14,9 +20,13 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @SpringBootApplication
 public class Application {
 
+
+
+	
     public static void main(String[] args) {
     	System.setProperty("spring.devtools.restart.enabled", "true"); 
         SpringApplication.run(Application.class, args);
+
     }
 
 
