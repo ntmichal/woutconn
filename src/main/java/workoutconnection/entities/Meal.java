0915f9125ad8bba1c57bda6657e541a0e719e693
Michał <nt.michal@gmail.com>
2018-10-12 16:25:52 +0200

    Clean up

--- src/main/java/workoutconnection/entities/Meal.java
+++ src/main/java/workoutconnection/entities/Meal.java
@@ -21,7 +21,7 @@ import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import workoutconnection.YearMonthDay;
+import workoutconnection.models.YearMonthDay;
 
 
 
