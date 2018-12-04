08c408cdb0e3985531502211350377d950cd1170
Michał <nt.michal@gmail.com>
2018-12-04 13:22:19 +0100

    Http Headers
    Added expose location, id

--- src/main/java/workoutconnection/config/CORSFilter.java
+++ src/main/java/workoutconnection/config/CORSFilter.java
@@ -10,17 +10,29 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
 
-public class CORSFilter implements Filter {
+import org.springframework.stereotype.Component;
 
+@Component
+public class CORSFilter implements Filter {
+	
+	@Override
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 		System.out.println("Filtering on...........................................................");
 		HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
-		response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+		response.setHeader("Access-Control-Allow-Methods", "POST,GET, PUT, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization, Origin, Accept, Access-Control-Request-Method, Access-Control-Request-Headers");
-
+        response.setHeader("Access-Control-Allow-Headers", 
+        		"X-Requested-With, "
+        		+ "Content-Type,"
+        		+ "Authorization, "
+        		+ "Origin, Accept, "
+        		+ "Access-Control-Request-Method, "
+        		+ "Access-Control-Request-Headers, "
+        		+ "location, Location");
+        response.addHeader("Access-Control-Expose-Headers", "Location, id");
+        response.setHeader("Access-Control-Expose-Headers", "Location, id");
 		chain.doFilter(req, res);
 	}
 
