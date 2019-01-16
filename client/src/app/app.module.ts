db50c22ee7c982d019ee2a64c1a3abbf08fab29d
Michał <nt.michal@gmail.com>
2019-01-16 13:14:37 +0100

    component remove
    -remove diet
    -remove edit
    -remove meal

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,16 +8,14 @@ import { HomeComponent } from './home/home.component';
 
 import { FormsModule } from '@angular/forms';
 import {RouteModule} from './route/route.module';
-import { DietComponent } from './diet/diet.component';
 import {Interceptor} from "./interceptor";
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {DietService} from './diet.service';
 import {TokenStorageService} from './token-storage.service';
 import {AuthService} from './auth.service';
-import { EditComponent } from './edit/edit.component';
+
 
 import { Globals } from './globals';
-import { MealComponent } from './meal/meal.component';
 
 import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
@@ -27,9 +25,6 @@ import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
     AppComponent,
     LoginComponent,
     HomeComponent,
-    DietComponent,
-    EditComponent,
-    MealComponent
   ],
   imports: [
     BrowserModule,
