24d1e619841497544d6d135805254f603f076445
Michał <nt.michal@gmail.com>
2019-04-16 12:47:49 +0200

    clean up
    removed useless MealInfoObject

--- src/main/java/workoutconnection/dao/IUserInfoDAO.java
+++ src/main/java/workoutconnection/dao/IUserInfoDAO.java
@@ -10,11 +10,8 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 
 import workoutconnection.entities.Measurement;
 import workoutconnection.entities.UserGoals;
-import workoutconnection.models.Exercise;
 
 public interface IUserInfoDAO {
-	
-
 
 	void saveWorkouts(List<Map<String, Object>> workouts, int userId)
 			throws JsonGenerationException, JsonMappingException, IOException;
@@ -28,7 +25,7 @@ public interface IUserInfoDAO {
 	public void insertMeasurement(Measurement measurement, int userId);
 	public void updateMeasurement(Measurement measurement);
 	public boolean deleteMeasurement(Measurement measurement);
-	
+
 	public void insertGoals(UserGoals userGoals, int userId);
 	public void updateGoals(UserGoals userGoals);
 	public void deleteGoals(UserGoals userGoals);
--- src/main/java/workoutconnection/service/IUserInfo.java
+++ src/main/java/workoutconnection/service/IUserInfo.java
@@ -13,16 +13,16 @@ import workoutconnection.entities.UserGoals;
 import workoutconnection.models.Exercise;
 
 public interface IUserInfo {
-	
-	public Object getUserInfo(int userid) 
+
+	public Object getUserInfo(int userid)
 			throws JsonParseException, JsonMappingException, IOException;
-	
+
 
 	public Object getWorkouts(int userid)
 			throws JsonParseException, JsonMappingException, IOException;
-	
 
-	
+
+
 	public void insertMeasurement(Measurement measurement, int userid);
 	public void updateMeasurement(Measurement measurement);
 	public boolean deleteMeasurement(Measurement measurement);
@@ -31,8 +31,9 @@ public interface IUserInfo {
 			throws JsonGenerationException, JsonMappingException, IOException;
 
 	public Object getMeasurement(int userid);
-	
-	public void insertGoals(UserGoals userGoals, int userid);
+
+	public void insertGoals(UserGoals userGoals);
+	public void insertGoals(UserGoals userGoals, int userId);
 	public void updateGoals(UserGoals userGoals);
 	public void deleteGoals(UserGoals userGoals);
 	public List<UserGoals> getAllGoals(int userid);
