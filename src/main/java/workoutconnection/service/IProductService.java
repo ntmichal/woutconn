4fb2a8d4627901e59010556dd6c1b8744e0cb37a
Michał <nt.michal@gmail.com>
2019-04-16 12:53:58 +0200

    update

--- src/main/java/workoutconnection/service/IProductService.java
+++ src/main/java/workoutconnection/service/IProductService.java
@@ -5,14 +5,14 @@ import java.util.List;
 import workoutconnection.entities.Product;
 
 public interface IProductService {
-	
+
 	List<Product> getAllProducts();
-	
+
 	Product getProduct(int id);
-	
+
 	Product insertProduct(Product product);
-	
+
 	void deleteById(int id);
-	
+
 	void update(Product product);
 }
