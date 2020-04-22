b6567757858ffe4e5d4a06f3a42c3c03d2b0cc2d
ntmichal <nt.michal@gmail.com>
2020-04-22 19:05:24 +0200

    Spock

--- src/test/groovy/UserServiceImplTest.groovy
+++ src/test/groovy/UserServiceImplTest.groovy
@@ -7,9 +7,13 @@ import workoutconnection.entities.User
 
 import javax.persistence.EntityManager
 import javax.persistence.criteria.CriteriaBuilder
+import javax.persistence.criteria.CriteriaDelete
 import javax.persistence.criteria.CriteriaQuery
 import javax.persistence.criteria.Root
+import javax.transaction.Transactional
 
+
+@Transactional
 @SpringBootTest
 class UserServiceImplTest extends Specification {
 
@@ -18,33 +22,58 @@ class UserServiceImplTest extends Specification {
 
     @Autowired
     private EntityManager entityManager;
-    User user;
+    User testUser1;
+    User testUser2;
     CriteriaBuilder criteriaBuilder;
     CriteriaQuery<Integer> criteriaQuery;
     Root<User> root;
 
     void setup(){
-        user = User.builder()
+        testUser1 = User.builder()
                 .setUsername("TestUser")
                 .setPassword("TestPassword")
                 .setEmail("testmail@testmail.com")
                 .build();
-
+        testUser2 = User.builder().setUsername("TestUser2")
+                .setPassword("TestPassword")
+                .setEmail("testmail@testmail.com")
+                .build();
         criteriaBuilder = entityManager.getCriteriaBuilder();
         criteriaQuery = criteriaBuilder.createQuery(Integer.class);
         root = criteriaQuery.from(User.class);
 
     }
 
+    void cleanup(){
+        criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaDelete<User> query = criteriaBuilder.createCriteriaDelete(User.class);
+        query.from(User.class);
+        entityManager.createQuery(query).executeUpdate();
+    }
+
     void 'test saving User, should return User with not null id'(){
         given:
-            user = userService.save(user);
+            testUser1 = userService.save(testUser1);
         when:
-            user.getId() != null;
+            testUser1.getId() != null;
         then:
             criteriaQuery.select(criteriaBuilder.count(root));
-            criteriaQuery.where(criteriaBuilder.equal(root.get("id"),user.getId()));
+            criteriaQuery.where(criteriaBuilder.equal(root.get("id"),testUser1.getId()));
             assert entityManager.createQuery(criteriaQuery).getSingleResult() == 1
 
     }
+
+    void 'return list of users'(){
+        given:
+            testUser1 = userService.save(testUser1)
+            testUser2 = userService.save(testUser2)
+            List<User> list = userService.findAll()
+        expect:
+            criteriaQuery.select(criteriaBuilder.count(root));
+            assert entityManager.createQuery(criteriaQuery).getSingleResult() == list.size()
+
+    }
+
+
+
 }
