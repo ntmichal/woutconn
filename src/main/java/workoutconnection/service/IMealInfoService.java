ad5b9bd5ba86fe7afaa38f0a97117e24e87396a7
Michał Maciocha <nt.michal@gmail.com>
2021-01-08 19:41:44 +0100

    meals by date. returns id, name, api lcoation

--- src/main/java/workoutconnection/service/IMealInfoService.java
+++ src/main/java/workoutconnection/service/IMealInfoService.java
@@ -2,7 +2,9 @@ package workoutconnection.service;
 
 
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 
 import workoutconnection.entities.Meal;
 
@@ -11,6 +13,7 @@ public interface IMealInfoService {
 
 	List<Meal> getAllMeals(int userId);
 	Meal getMeal(int id);
+	List<Map<String,Object>> getMealByDate(int userId, LocalDate mealDate);
 	void deleteMeal(int id);
 	void insertMeal(Meal meal, int userId);
 	void updateMeal(Meal meal);
