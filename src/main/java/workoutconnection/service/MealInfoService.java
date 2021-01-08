89aa6b1881bef55629bd25ca9c0b41e62c9a5e9f
Michał Maciocha <nt.michal@gmail.com>
2021-01-08 19:41:44 +0100

    meals by date. returns id, name, api lcoation

--- src/main/java/workoutconnection/service/MealInfoService.java
+++ src/main/java/workoutconnection/service/MealInfoService.java
@@ -1,7 +1,11 @@
 package workoutconnection.service;
 
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,6 +29,34 @@ public class MealInfoService implements IMealInfoService {
 	@Override
 	public List<Meal> getAllMeals(int userId) {
 		return mealInfoDAO.getAllMeals(userId);
+	}
+
+
+	@Override
+	public List<Map<String,Object>> getMealByDate(int userId, LocalDate mealDate) {
+		List<Meal> mealList = mealInfoDAO.getMealByDate(userId, mealDate);
+
+		List<Map<String,Object>> mealsLinksMapList = new ArrayList<>();
+
+
+		mealList.stream().forEach( meal -> {
+			StringBuilder apiLinkToMeal = new StringBuilder();
+			apiLinkToMeal.append("/api/meal/");
+			apiLinkToMeal.append(userId);
+			apiLinkToMeal.append("/id/");
+			apiLinkToMeal.append(meal.getId());
+
+			Map<String, Object> mealsLinksMap = new HashMap<>();
+			mealsLinksMap.put("id", meal.getId());
+			mealsLinksMap.put("name", meal.getName());
+			mealsLinksMap.put("link", apiLinkToMeal.toString());
+
+			mealsLinksMapList.add(mealsLinksMap);
+
+		});
+
+		return mealsLinksMapList;
+
 	}
 
 	@Override
