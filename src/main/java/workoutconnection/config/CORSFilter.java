936106904a03c7b13b817a3bc8ee45fe35e9292a
Michał Maciocha <nt.michal@gmail.com>
2021-02-09 14:41:25 +0100

    fix allow method added , betwen method

--- src/main/java/workoutconnection/config/CORSFilter.java
+++ src/main/java/workoutconnection/config/CORSFilter.java
@@ -29,7 +29,7 @@ public class CORSFilter implements Filter {
 		HttpServletRequest request = (HttpServletRequest) req;
 		response.addHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Credentials", "true");
-		response.setHeader("Access-Control-Allow-Methods", "GET POST PUT DELETE OPTIONS");
+		response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers",
 				"Origin, x-requested-with, Authorization, x-auth-token, origin, Content-Type, accept"
